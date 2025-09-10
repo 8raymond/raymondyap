fa352aa7e7e7cffaa10a3031111bb3ea78fa4707
8raymond <135733942+8raymond@users.noreply.github.com>
2025-09-10 14:57:46 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Raymond Yap",
+  title: "Mechanical Engineer",
+  description: "Portfolio website of Raymond Yap",
   accentColor: "#1d4ed8",
   social: {
     email: "your-email@example.com",
