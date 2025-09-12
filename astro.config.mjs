b212f4485d2ea083885613a9e733abeac8d9bb28
8raymond <135733942+8raymond@users.noreply.github.com>
2025-09-12 09:49:40 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://8raymond.github.io/raymondyap/',
-  base: '/raymondyap',
-  plugins: [tailwindcss()],
+  site: "https://8raymond.github.io/raymondyap/",
+  base: "/raymondyap",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
