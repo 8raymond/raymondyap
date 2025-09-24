ee7261d5949d969dde15d4221e87509cef0f5f5f
8raymond <135733942+8raymond@users.noreply.github.com>
2025-09-24 16:34:52 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -48,7 +48,7 @@ export const siteConfig = {
       name: "International Space Station Research Lab Payload(s)",
       description:
         "Executed and fully designed 3 autonomous microlab experiments sent aboard the International Space Station; published and presented papers on 2 experiments Microgravity Induced Corrosion and N. Muscorum Growth in Microgravity for ASGSR, awarded 2nd Place in Poster Competition project. Served as Team Lead for 7+ members, delegating tasks and organizing benchmark design reviews",
-      link: "https://drive.google.com/file/d/18Fnc8fem6kONJT607lPGBtnCrQUT5Rqn/view?usp=sharing",
+      link: "https://drive.google.com/drive/folders/1VMqEFfHcMA1pWUtIYVN5VuF1bzJTtQHz?usp=sharing",
       skills: ["Team Leadership", "CAD", "Electrical Schematics", "Arduino", "3D Printing"],
     },
   ],
